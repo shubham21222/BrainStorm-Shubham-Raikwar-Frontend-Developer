24ba473c8f8f28163fe11748453c781b99a3fa7a
shubham21222 <shubhamraikwar@hotmail.com>
2024-04-08 15:25:14 +0530

    added app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
